ab79266c38d5c3ba8269734cacce2f5869ac288f
Kay Stenschke <info@stenschke.com>
2015-01-29 22:55:38 +0100

    commented, cleanup

--- MantisUiEnhanced/MantisUiEnhanced.php
+++ MantisUiEnhanced/MantisUiEnhanced.php
@@ -13,33 +13,56 @@
 
 class MantisUiEnhancedPlugin extends MantisPlugin {
 
+    /**
+     * Register plugin
+     */
 	function register() {
 		$this->name = 'Mantis UI Enhanced';
 		$this->description = 'Enhances the MantisBT UI usability (makes dropdowns submit on-change, adds label-tags etc.)';
 
 		$this->version = '0.0.1';
 		$this->requires = array(
-			'MantisCore'	=> '1.2.0, < 1.3', # Make it available only to 1.2.x
-			'jQuery'		=> '1.11.1',	// @see https://github.com/mantisbt-plugins/jquery
+			'MantisCore' => '1.2.0, < 1.3', # Make it available only to 1.2.x
+			'jQuery'     => '1.11.1', // @see https://github.com/mantisbt-plugins/jquery
 		);
 
-		$this->author	= 'Kay Stenschke';
-		$this->contact	= 'kstenschke@coexec.com';
-		$this->url		= 'https://github.com/coexec/mantisbt-uienhanced';
+		$this->author   = 'Kay Stenschke';
+		$this->contact  = 'kstenschke@coexec.com';
+		$this->url      = 'https://github.com/coexec/mantisbt-uienhanced';
 	}
 
+    /**
+     * @return array
+     */
 	function config() {
 		return array();
 	}
-	
+
+    /**
+     * @return array    List of hooks and their callbacks in this plugin
+     */
 	function hooks() {
 		return array(
-			'EVENT_LAYOUT_RESOURCES' => 'resources',	// appended into <head>
+			'EVENT_LAYOUT_RESOURCES' => 'resources',	// returns content to be appended into <head>
 		);
 	}
 
+    /**
+     * @param   String  $p_event
+     * @return  String  Content to be added to <head> section
+     */
 	function resources( $p_event ) {
         include( 'lang/strings_' . lang_get_current() . '.txt' );
+
+        /** @var String $s_email_reproducibility */
+        /** @var String $s_email_severity */
+        /** @var String $s_email_priority */
+        /** @var String $s_steps_to_reproduce */
+        /** @var String $s_select_profile */
+        /** @var String $s_or_fill_in */
+        /** @var String $s_additional_information */
+        /** @var String $s_view_status */
+        /** @var String $s_report_stay */
 
 		return '<script type="text/javascript">' /* Define localized labels to allow UiEnhancer sizzle to select element via innerHTML */ . '
                 UiEnhancedLabels = {
@@ -55,6 +78,5 @@ class MantisUiEnhancedPlugin extends MantisPlugin {
                 };</script>'
              . '<script type="text/javascript" src="' . plugin_file( 'ui-enhanced.js' ) . '"></script>';
 	}
-
 
 }
